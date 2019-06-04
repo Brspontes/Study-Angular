243d69ec85c5a40fa3dbe54ff015258f1a0c409e
Brian Pontes <brian.robert16@hotmail.com>
2019-06-03 23:12:45 -0300

    Adicionando modulo photos, criando exportação para acesso externo, aprendendo sobre o ng for

--- alurapic/src/app/app.component.ts
+++ alurapic/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    
+    photos = [
+      {
+        url: '../assets/images/7683420775.jpg',
+        description: 'Leão1'
+      },
+      {
+        url: '../assets/images/8609251160.jpg',
+        description: 'Leão1'
+      }
+    ];
 }
